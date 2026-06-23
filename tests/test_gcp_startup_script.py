52c7bdc935f610078145919167decebf5e9eb88a
kingyx3 <kingyx3@hotmail.com>
2026-06-23 12:57:13 +0800

    Fix startup guard test lint

--- tests/test_gcp_startup_script.py
+++ tests/test_gcp_startup_script.py
@@ -28,15 +28,24 @@ def test_ibc_uses_vm_local_config_and_gateway_install() -> None:
     assert 'TWS_PATH": tws_path' in script
     assert 'TWS_SETTINGS_PATH": "/home/poma/Jts"' in script
     assert 'LOG_PATH": "/home/poma/ibc/logs"' in script
-    assert "find \"$${IB_GATEWAY_DIR}\" -type d -path '*/ibgateway/[0-9]*/jars'" in script
+    assert (
+        "find \"$${IB_GATEWAY_DIR}\" -type d "
+        "-path '*/ibgateway/[0-9]*/jars'"
+    ) in script
 
 
 def test_ibc_credentials_remain_vm_local_and_private() -> None:
     script = _startup()
 
-    assert "install -d -m 700 -o poma -g poma \"$${IBC_HOME}\" \"$${IBC_HOME}/logs\"" in script
-    assert "install -m 600 -o poma -g poma \"$${IBC_DIR}/config.ini\" \"$${IBC_CONFIG}\"" in script
-    assert "chmod 600 \"$${IBC_CONFIG}\"" in script
+    assert (
+        'install -d -m 700 -o poma -g poma '
+        '"$${IBC_HOME}" "$${IBC_HOME}/logs"'
+    ) in script
+    assert (
+        'install -m 600 -o poma -g poma '
+        '"$${IBC_DIR}/config.ini" "$${IBC_CONFIG}"'
+    ) in script
+    assert 'chmod 600 "$${IBC_CONFIG}"' in script
     assert "IBKR password" in script
     assert "GCP_BOOTSTRAP_SERVICE_ACCOUNT_KEY" not in script
     assert "TELEGRAM_BOT_TOKEN" not in script
